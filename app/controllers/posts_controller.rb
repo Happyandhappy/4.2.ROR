d2571eb32e554c1751bf2726c22e2b28d453dafd
happyandhappy <greyfrapps@gmail.com>
2018-04-09 10:15:46 +0100

    Add "post delete, add, delete comment to post"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	http_basic_authenticate_with name: "brad", password:"1234", except: [:index, :show]
+
     def index
     	@posts = Post.all
     end
@@ -9,7 +11,7 @@ class PostsController < ApplicationController
 
     def new
     	@post = Post.new
-        # @title = "Add Post"
+        @title = "Add Post"
     end
 
     def create
@@ -22,7 +24,30 @@ class PostsController < ApplicationController
         end
     end
 
-    private def post_params
-    	params.require(:post).permit(:title, :body)
-    end
+	def edit
+		@post = Post.find(params[:id])
+		@title = "Eidt Post"
+	end 
+
+	def update
+		@post = Post.find(params[:id])
+
+		if (@post.update(post_params))
+			redirect_to @post
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+
+	end
+
+	private def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
